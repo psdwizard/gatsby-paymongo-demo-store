9dde125a5596b4749bec2d716e412387366e6532
psdwizard <peachy@fullstack.ph>
2020-03-19 10:56:23 +0800

    category listing, wip home

---
+++ src/components/CategoryListing.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+
+function CategoryListing ({category}) {
+  return (
+      <div className="section-category-listing">
+        <div className="container-fluid">
+          <div className="category-listing">
+          {category.map((item, i) => {
+            let description = item.description;
+            let dotsDescription = description.length > 60 ? '...' : '';
+            let excerptedDescription = description.slice(0, 60) + dotsDescription;
+
+            return (
+              <div className="category-item" key={i}>
+                <div className="category-text-wrapper">
+                  <h3 className="category-title">{item.title}</h3>
+                  <p className="category-description">{excerptedDescription}</p>
+                  <Link to={item.url} className="btn btn-black btn-no-border category-button">Discover</Link>
+                </div>
+                <div className="category-image-wrapper">
+                  <img src={item.image} className="category-image" alt={item.imageName}/>
+                </div>
+              </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default CategoryListing
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "../components/Header"
 import SwiperBanner from "../components/SwiperBanner"
+import CategoryListing from "../components/CategoryListing"
 // import { Link } from "gatsby"
 
 
@@ -15,12 +16,36 @@ const swiperData = [
   },
   {
     title: "Lorem ipsum dolor sit amet",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
     buttonText: "Shop Now",
     buttonLink: "/",
     image: require("../images/../assets/images/bootstrap-illustration-12.png"),
     imageName: "image",
   },
+]
+
+const categoryData = [
+  {
+    title: 'Mascots',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    url: '/',
+    image: require("../images/../assets/images/bootstrap-illustration-12.png"),
+    imageName: 'mascot image'
+  },
+  {
+    title: 'Mascots',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    url: '/',
+    image: require("../images/../assets/images/bootstrap-illustration-12.png"),
+    imageName: 'mascot image'
+  },
+  {
+    title: 'Mascots',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    url: '/',
+    image: require("../images/../assets/images/bootstrap-illustration-3.png"),
+    imageName: 'mascot image'
+  }
 ]
 
 
@@ -29,5 +54,6 @@ export default () =>
     <Header />
     <main className="home">
       <SwiperBanner content={swiperData} />
+      <CategoryListing category={categoryData} />
     </main>
   </div>
