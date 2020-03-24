2dba060228955326f69e28a18be7372ee19a0769
psdwizard <peachy@fullstack.ph>
2020-03-24 16:32:56 +0800

    update success-payment.js and failed-payment.js

--- src/pages/failed-payment.js
+++ src/pages/failed-payment.js
@@ -1,10 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const failedPayment = () => {
   return (
     <div>
-      FAILED PAYMENT!
+      <Header />
+      <main className="success-payment">
+        <div className="container">
+          <img className="mascot" src={require('../assets/images/Bootstrap Sad.png')} alt="fs success"/>
+          <div className="text-wrapper">
+            <h2>Payment not accepted</h2>
+            <p>Please try again or use another method</p>
+            <Link to="/checkout" className="btn-swipe-black hover-swipe-right">Back</Link>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   )
 }
--- src/pages/success-payment.js
+++ src/pages/success-payment.js
@@ -1,10 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const successPayment = () => {
   return (
     <div>
-      SUCCESS PAYMENT
+      <Header />
+      <main className="success-payment">
+        <div className="container">
+          <img className="mascot" src={require('../assets/images/Bootstrap Good job.png')} alt="fs success"/>
+          <div className="text-wrapper">
+            <h2>Payment Successful</h2>
+            <p>Thanks for your purchase!</p>
+            <Link to="/product-catalog" className="btn-swipe-black hover-swipe-right">Continue Shopping</Link>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   )
 }
