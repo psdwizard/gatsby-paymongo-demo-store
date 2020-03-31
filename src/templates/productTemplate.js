dfe2907d5a4987d15894ea4ee6374736239d3749
psdwizard <peachy@fullstack.ph>
2020-03-31 19:58:30 +0800

    add to cart modal trigger

--- src/templates/productTemplate.js
+++ src/templates/productTemplate.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { Link, graphql } from "gatsby"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faFacebookF, faLinkedinIn} from "@fortawesome/free-brands-svg-icons"
+import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import {Helmet} from "react-helmet"
 
 export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
+  data // this prop will be injected by the GraphQL query below.
+}, props ) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter } = markdownRemark
   const [cartList, setCartList] = useState({
@@ -44,6 +45,46 @@ export default function Template({
       'qtty' : value 
     })
   }
+
+  const {
+    className
+  } = props;
+
+  const [, updateState] = React.useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+  let cartItems = []
+  let totalAmount = 0;
+  let totalQuantity = 0;
+
+  const removeItem = item => {
+    const selectedItem = cartItems.findIndex(x => x.setID === item.setID)
+      if (selectedItem > -1) {
+        cartItems.splice(selectedItem, 1)
+        localStorage.setItem('cartList', JSON.stringify(cartItems))
+      }
+    forceUpdate()
+  }
+  
+  const sumProperty = (arr, type) => {
+      return arr.reduce((total, obj) => {
+      if (typeof obj[type] === 'string') {
+        return total + Number(obj[type]);
+      }
+      return total + obj[type];
+    }, 0);
+  }
+
+  if (typeof window !== `undefined`) {
+    cartItems = JSON.parse(localStorage.getItem('cartList'))
+    if (cartItems !== null) {
+      totalAmount = ( sumProperty(cartItems, 'totalPrice') ).toFixed(2); 
+      totalQuantity = sumProperty(cartItems, 'qtty'); 
+      localStorage.setItem('total', JSON.stringify(totalAmount))
+    }
+  }
+
 
   useEffect(() => {
     if ( cartList.productName === "" || cartList.cartPressed === false ) {
@@ -94,8 +135,10 @@ export default function Template({
                     <option>2</option>
                     <option>3</option>
                   </select>
-                  <button onClick={() => handleClick(frontmatter)} 
-                    className="btn-swipe-black hover-swipe-right">
+                  <button
+                    className="btn-swipe-black hover-swipe-right"
+                    onClick={() => { handleClick(frontmatter); toggle();}}
+                    >
                     Add to Cart
                   </button>
                 </div>
@@ -111,10 +154,6 @@ export default function Template({
                     <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
                   </Link>
                 </div>
-                {/* <div className="category-list">
-                  <FontAwesomeIcon icon={faTag} className="tag-icon" />
-                  Desk Lamps, Study Lamps, Bed Lamps
-                </div> */}
               </div>
             </div>
             <Tabs className="tabs-wrapper">
@@ -196,6 +235,51 @@ export default function Template({
             </div>
           </div>
         </div>
+
+        <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Your Cart</ModalHeader>
+        <ModalBody>
+          <ul className="cart-product-listing">
+            {
+              cartItems ? 
+                cartItems.map((item, i) => {
+                  return (
+                    <li key={i} className="cart-product-item">
+                      <div className="image-wrapper">
+                        <div className="thumbnail-holder">
+                          <img src={item.image1024} className="product-image" alt={item.productName} />
+                        </div>
+                        <Button color="link" onClick={() => removeItem(item)}>Remove</Button>
+                      </div>
+                      <div className="text-wrapper">
+                      <h4 className="product-name">{item.productName}</h4>
+                      <p className="computation">{item.qtty} x PHP {item.price} = PHP {(item.totalPrice).toFixed(2)}</p>
+                      </div>                  
+                    </li>
+                  )
+                })
+              : 'Your cart is empty'
+            }
+          
+          </ul>
+        </ModalBody>
+        {
+          cartItems ? 
+          <>
+            <div className="total-holder">
+              <h3 className="total">
+                Total: {} 
+                PHP {} {totalAmount}
+                </h3>
+            </div>
+            <div className="btn-holder">
+              <Link to="/checkout" className="btn-swipe-black hover-swipe-right btn-checkout">Checkout</Link>
+              <Link to="/products" className="btn-swipe-black hover-swipe-right">Continue Shopping</Link>
+            </div>
+          </>
+          : ''
+        }
+      </Modal>
      </main>
      <Footer />
     </div>
